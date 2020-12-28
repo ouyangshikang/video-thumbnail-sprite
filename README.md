6048348cfffabb675d30b2ae3eae423ba8e03b5a
baituo <baituo@gaoding.com>
2020-12-28 18:30:25 +0800

    feat: init VideoThumbnail

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # video-thumbnail-sprite
-视频取帧生成雪碧图
+视频取帧，生成雪碧图
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,49 @@
-export function add(a: number, b: number) {
-    return a + b;
+// 测试视频
+const video = 'https://gd-filems.dancf.com/mps-output/712778ba01364c9cbc334e0b0509ac4d/to-mp4/file.mp4';
+
+
+export class VideoThumbnail {
+    /**
+     * 视频元素
+     */
+    private videoElement: HTMLVideoElement;
+
+    /**
+     * canvas 元素
+     */
+    private canvas: HTMLCanvasElement;
+
+    /**
+     * canvas 上下文
+     */
+    private canvasContext: CanvasRenderingContext2D;
+
+    /**
+     * 预览图数组
+     */
+    private previewImages: string[]
+
+    /**
+     * @param videoUrl 视频地址
+     */
+    constructor(videoUrl: string) {
+        if(!videoUrl) {
+            throw new Error('video url required');
+        }
+
+        this.videoElement = document.createElement('video');
+        this.canvas = document.createElement('canvas');
+        const context = this.canvas.getContext('2d');
+
+        if(!context) {
+            throw new Error('Could not create canvas context')
+        }
+
+        this.canvasContext = context;
+        this.videoElement.src = videoUrl;
+    }
+
+
+
+
 }
